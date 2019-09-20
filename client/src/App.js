b6abd62bb1a0fe3d1d5c9383c6b77fcad53dd220
yongchengzhu <yongchengzhu002@gmail.com>
2019-09-19 23:43:30 -0400

    Setup React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, R } from 'react-router-dom';
+import { Redirect } from 'react-router';
+
+import Navigation from './components/Navigation';
+import Signup     from './components/Signup';
+import Signin     from './components/Signin';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Welcome to Budgetbase!
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <Route path="/" render={ () => <Redirect to="/signup" /> } />
+          <Route path="/signup" component={ Signup } />
+          <Route path="/signin" component={ Signin } />
+        </div>
+      </Router>
     );
   }
 }
