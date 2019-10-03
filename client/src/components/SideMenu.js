48c3c4eefcf15a58271e80c5a1bb783b2097855e
JiseonYu <49763204+JiseonYu@users.noreply.github.com>
2019-10-03 01:19:33 -0400

    Add files via upload

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-class SideMenu extends React.Component {
-  render() {
-    return (
-      <div>Side Menu</div>
-    );
-  }
+class SideMenu extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>
+                    <label for="Date"></label>
+                    <input id="Date" type="date" />
+                    <button type="button">Search</button>
+                </p>
+                <p>
+                    <button type="button">Add Budget</button>
+                </p>
+                    <button type="button">Reports</button>
+                <p>
+                    <button type="button">Checking Account</button>
+                </p>
+                <p>
+                    <button type="button">Import/Export</button>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default SideMenu;
+
+
