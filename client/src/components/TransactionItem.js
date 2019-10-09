7f3d6692e9931e20ba57413c4b79a3d98c1de119
Leji-Li-93 <leji.li.93@outlook.com>
2019-10-09 17:40:47 -0400

    Delete form

--- client/src/components/TransactionItem.js
+++ client/src/components/TransactionItem.js
@@ -1,57 +1,86 @@
 import React, { Component } from 'react'
+import DeleteItemForm from './forms/DeleteItemForm'
 
 // ------------------------------------------------------------------
 // functional Components
 // ------------------------------------------------------------------
 
-const TransactionItem = ( {transaction: {name, createdAt, category, cost}}) => {
-    return (
+class TransactionItem extends React.Component{
+
+  handleModal = () =>{
+    this.props.setModalTitle(null);
+    this.props.setModalContent(<DeleteItemForm toggleModal = {this.props.toggleModal}/>);
+    this.props.toggleModal();
+  }
+
+  render(){
+    return(
       <div className="card-panel item-container">
         <div className="row item">
-          <div className="col s2">{new Date(createdAt).toDateString()}</div>
-          <div className="col s2">{name}</div>
-          <div className="col s2">${cost}</div>
-          <div className="col s3">{category}</div>
+          <div className="col s2">{new Date(this.props.transaction.createdAt).toDateString()}</div>
+          <div className="col s2">{this.props.transaction.name}</div>
+          <div className="col s2">${this.props.transaction.cost}</div>
+          <div className="col s3">{this.props.transaction.category}</div>
           <div className="col s1">
             <i className="material-icons edit">edit</i>
           </div>
           <div className="col s1">
-            <i className="material-icons delete">delete</i>
+            <i className="material-icons delete" onClick = {this.handleModal}>delete</i>
           </div>
         </div>
       </div>
-    );
-    // const style = {
-    //    border: '0.5px solid lightgray'
-    // }
-    // return (
-    //     <div className = "TransactionItem" style = {style}>
-    //         <p></p>
-    //         <form className = "form" style = {style}>
-    //             <div className="row" >
-    //                 <div className ="col s12 m3">
-    //                     <p>{name}</p>
-    //                 </div>
-    //                 <div className="col s12 m2" >
-    //                     <p>{date}</p>
-    //                 </div>
-    //                 <div className="col s12 m3" >
-    //                     <p>{type}</p>
-    //                 </div>
-    //                 <div className ="col s12 m2"  >
-    //                     <p>{cost+ ' $'} </p>
-    //                 </div> 
-    //                 <div className ="col s12 m1"  >
-    //                     <i className="fas fa-edit"></i>
-    //                 </div> 
-    //                 <div className ="col s12 m1"  >
-    //                     <i className="fas fa-eraser"></i>
-    //                 </div> 
-    //             </div>
-    //         </form>
-    //     </div> 
-    // )
+    )
+  }
 }
+export default TransactionItem;
+// const TransactionItem = ( {transaction: {name, createdAt, category, cost}}) => {
+//     return (
+//       <div className="card-panel item-container">
+//         <div className="row item">
+//           <div className="col s2">{new Date(createdAt).toDateString()}</div>
+//           <div className="col s2">{name}</div>
+//           <div className="col s2">${cost}</div>
+//           <div className="col s3">{category}</div>
+//           <div className="col s1">
+//             <i className="material-icons edit">edit</i>
+//           </div>
+//           <div className="col s1">
+//             <i className="material-icons delete">delete</i>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//     // const style = {
+//     //    border: '0.5px solid lightgray'
+//     // }
+//     // return (
+//     //     <div className = "TransactionItem" style = {style}>
+//     //         <p></p>
+//     //         <form className = "form" style = {style}>
+//     //             <div className="row" >
+//     //                 <div className ="col s12 m3">
+//     //                     <p>{name}</p>
+//     //                 </div>
+//     //                 <div className="col s12 m2" >
+//     //                     <p>{date}</p>
+//     //                 </div>
+//     //                 <div className="col s12 m3" >
+//     //                     <p>{type}</p>
+//     //                 </div>
+//     //                 <div className ="col s12 m2"  >
+//     //                     <p>{cost+ ' $'} </p>
+//     //                 </div> 
+//     //                 <div className ="col s12 m1"  >
+//     //                     <i className="fas fa-edit"></i>
+//     //                 </div> 
+//     //                 <div className ="col s12 m1"  >
+//     //                     <i className="fas fa-eraser"></i>
+//     //                 </div> 
+//     //             </div>
+//     //         </form>
+//     //     </div> 
+//     // )
+// }
 
 
 // ------------------------------------------------------------------
@@ -97,4 +126,4 @@ const TransactionItem = ( {transaction: {name, createdAt, category, cost}}) => {
 //     }
 // }
 
-export default TransactionItem
+//export default TransactionItem
