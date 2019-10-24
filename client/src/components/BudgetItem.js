85ea7e1ef592dfb294d427a9c286d2458ccbb39f
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-10-24 13:02:26 -0400

    Implemented conditionally rendering for the BudgetItem Component. (If props.budget is null then render a + button. Otherwise render the budget name and amount)

--- client/src/components/BudgetItem.js
+++ client/src/components/BudgetItem.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
 class BudgetItem extends React.Component {
-  render() {
-    const bg = this.props.idx % 2? "teal-bg" : "light-blue-bg";
-    const btnbg = this.props.idx % 2? "light-blue-bg" : "teal-bg"
-    return (
-      <div className="budget-item-container col s4">
-        <div className={`budget-item ${bg}`}>
-          <div class={`${btnbg} add-btn btn-floating btn-large waves-light`}>
-            <i class="material-icons">add</i>
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        const {name, amount} = this.props;
+        
+        const bg = this.props.idx % 2? "teal-bg" : "light-blue-bg";
+        const btnbg = this.props.idx % 2? "light-blue-bg" : "teal-bg"
+        return (
+            <div className="budget-item-container col s4">
+                <div>
+                    {name ? (
+                        <div className={`budget-item ${bg}`}>
+                            <div class = "dashboard-title white-text balance center">{name}</div>
+                            <div class = "dashboard-title white-text balance center">$ {amount}</div>
+                        </div> ) : (
+                        <div className={`budget-item ${bg}`}>
+                        <div class={`${btnbg} add-btn btn-floating btn-large waves-light`}>
+                            <i class="material-icons">add</i>
+                        </div>
+                    </div> 
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default BudgetItem;
