1b40bcfa7eaa991f8c65e770535092ff5bbfda71
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-10-24 12:59:40 -0400

    Added state to the BudgetContainer with dummy data for budgets.

--- client/src/components/BudgetContainer.js
+++ client/src/components/BudgetContainer.js
@@ -2,15 +2,26 @@ import React from 'react';
 import BudgetItem from './BudgetItem';
 
 class BudgetContainer extends React.Component {
-  render() {
-    return (
-      <div className="row">
-        <BudgetItem idx={0} />
-        <BudgetItem idx={1} />
-        <BudgetItem idx={2} />
-      </div>
-    );
-  }
+    
+    state = {
+        budgets:
+        [
+            {name : "Budget a", amount : 300},
+            {name : "Budget b", amount : 500},
+            {name : "Budget c", amount : 600}
+        ]
+    };
+
+    render() {
+        return (
+        <div className="row">
+            {/* <div>{this.state.budgets[0].name}</div> */}
+            <BudgetItem idx={0}  name = {this.state.budgets[0].name} amount = {this.state.budgets[0].amount}  /> 
+            <BudgetItem idx={0}  name = {this.state.budgets[1].name} amount = {this.state.budgets[1].amount}  /> 
+            <BudgetItem idx={0}   amount = {this.state.budgets[2].amount}  /> 
+        </div>
+        );
+    }
 }
 
 export default BudgetContainer;
