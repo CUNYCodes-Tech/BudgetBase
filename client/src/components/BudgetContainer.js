415166bf23085e8b641bb226eb5b11a71c89af21
yongchengzhu <yongchengzhu002@gmail.com>
2019-10-24 15:28:40 -0400

    Improved style of budget.

--- client/src/components/BudgetContainer.js
+++ client/src/components/BudgetContainer.js
@@ -2,26 +2,25 @@ import React from 'react';
 import BudgetItem from './BudgetItem';
 
 class BudgetContainer extends React.Component {
-    
-    state = {
-        budgets:
-        [
-            {name : "Budget a", amount : 300},
-            {name : "Budget b", amount : 500},
-            {name : "Budget c", amount : 600}
-        ]
-    };
+  state = {
+    budgets:
+    [
+      {name : "Car of My Dream", amount : 300},
+      {name : "Meow", amount : 500},
+      {name : "Budget c", amount : 600}
+    ]
+  };
 
-    render() {
-        return (
-        <div className="row">
-            {/* <div>{this.state.budgets[0].name}</div> */}
-            <BudgetItem idx={0}  name = {this.state.budgets[0].name} amount = {this.state.budgets[0].amount}  /> 
-            <BudgetItem idx={0}  name = {this.state.budgets[1].name} amount = {this.state.budgets[1].amount}  /> 
-            <BudgetItem idx={0}   amount = {this.state.budgets[2].amount}  /> 
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="row">
+        {/* <div>{this.state.budgets[0].name}</div> */}
+        <BudgetItem idx={0}  name = {this.state.budgets[0].name} amount = {this.state.budgets[0].amount}  /> 
+        <BudgetItem idx={0}  name = {this.state.budgets[1].name} amount = {this.state.budgets[1].amount}  /> 
+        <BudgetItem idx={0}   amount = {this.state.budgets[2].amount}  /> 
+      </div>
+    );
+  }
 }
 
 export default BudgetContainer;
--- client/src/components/BudgetItem.js
+++ client/src/components/BudgetItem.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
 class BudgetItem extends React.Component {
-    render() {
-        const {name, amount} = this.props;
-        
-        const bg = this.props.idx % 2? "teal-bg" : "light-blue-bg";
-        const btnbg = this.props.idx % 2? "light-blue-bg" : "teal-bg"
-        return (
-            <div className="budget-item-container col s4">
-                <div>
-                    {name ? (
-                        <div className={`budget-item ${bg}`}>
-                            <div class = "dashboard-title white-text balance center">{name}</div>
-                            <div class = "dashboard-title white-text balance center">$ {amount}</div>
-                        </div> ) : (
-                        <div className={`budget-item ${bg}`}>
-                        <div class={`${btnbg} add-btn btn-floating btn-large waves-light`}>
-                            <i class="material-icons">add</i>
-                        </div>
-                    </div> 
-                    )}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const {name, amount} = this.props;
+    
+    const bg = this.props.idx % 2? "teal-bg" : "light-blue-bg";
+    const btnbg = this.props.idx % 2? "light-blue-bg" : "teal-bg"
+    return (
+      <div className="budget-item-container col s4">
+        <div>
+          {name ? (
+            <div className={`budget-item ${bg}`}>
+              <div className="budget-title">{name}</div>
+              <div className="budget-amount">${amount}</div>
+              <div className="progress-bar"></div>
+            </div> ) : (
+            <div className={`budget-item ${bg}`}>
+              <div className={`${btnbg} add-btn btn-floating btn-large waves-light`}>
+                <i className="material-icons">add</i>
+              </div>
+            </div> 
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default BudgetItem;
