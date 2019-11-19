8664043dfa84a3d676c6bb16b4c22e58f31b5ff9
Jorge Roldan <jorgeroldanroldan@gmail.com>
2019-11-19 10:36:04 -0500

    Created component for the Archive page

---
+++ client/src/components/Archive.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Archive extends React.Component {
+  render() {
+    return (
+        <div className = "center">
+            Archive
+        </div>
+    );
+  }
+}
+
+export default Archive;
